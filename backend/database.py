8c18cf11621853f5ccb1872c76649811d3dbae08
Tandon <tjdjj12@gmail.com>
2022-09-29 09:55:46 -0600

    comit

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@
 import motor.motor_asyncio
 from model import Todo
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+client = motor.motor_asyncio.AsyncIOMotorClient()
 database = client.TodoList
 collection = database.todo
 
